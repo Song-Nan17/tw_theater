4625b303a0d53ad5ef98b6511233b9287478c385
Song <songnan17@qq.com>
2019-05-11 20:38:21 +0800

    Project init.

---
+++ src/main/java/com/example/tw_theater/TwTheaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.tw_theater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwTheaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwTheaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tw_theater/TwTheaterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tw_theater;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwTheaterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
