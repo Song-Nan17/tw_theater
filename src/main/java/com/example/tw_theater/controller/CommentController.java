a29e9a3ce8bafbf80c9bf95a55424d4e0305f2bc
yeland <wlx100715@163.com>
2019-05-14 18:40:49 +0800

    Delete some unuseful code.

--- src/main/java/com/example/tw_theater/controller/CommentController.java
+++ src/main/java/com/example/tw_theater/controller/CommentController.java
@@ -7,17 +7,10 @@ import com.example.tw_theater.model.Comment;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 99a4f6c3ef8789a0b6f9b578a25b3048f0d5ac5a
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
